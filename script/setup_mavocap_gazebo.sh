47c52d53b7b74d9736561952538279996df3a7fc
ootang2018 <yutang.liu2018@outlook.com>
2020-06-21 19:35:47 +0200

    rosparam

--- script/setup_mavocap_gazebo.sh
+++ script/setup_mavocap_gazebo.sh
@@ -8,16 +8,13 @@ gui=true
 
 # spawn robot and world
 echo "Launching Gazebo..."
-screen -d -m -S GAZEBO bash -i -c "conda activate py2"
-screen -d -m -S GAZEBO bash -i -c "roslaunch blimp_description blimp_with_env.launch world_name:=$WORLD enable_wind:=$enable_wind  gui:=$gui --screen"
+screen -dm -S BLIMP screen sh -c "conda activate py2; roslaunch blimp_description blimp_with_env.launch world_name:=$WORLD enable_wind:=$enable_wind  gui:=$gui --screen"
 
 sleep 10
 
 # spawn target
 echo "Spawning target"
-screen -d -m -S TARGET bash -i -c "conda activate py2"
-screen -d -m -S TARGET bash -i -c "roslaunch blimp_description spawn_target.launch --screen"
-
+screen -dm -S TARGET screen sh -c "conda activate py2; roslaunch blimp_description spawn_target.launch"
 
 date
 # exit 0
